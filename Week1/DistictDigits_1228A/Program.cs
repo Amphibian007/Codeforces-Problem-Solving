6d00e6276343b2a88da9b1cdfcf10c1c8dbcd094
Dipto Das <diptod111@gmail.com>
2020-09-21 21:56:41 +0600

    Added Distinct Digit Problem

---
+++ Week1/DistictDigits_1228A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace DistictDigits_1228A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            for (var i = input[0]; i <= input[1]; i++)
+            {
+                if (i.ToString().Length == i.ToString().Distinct().ToArray().Length)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine(-1);
+        }
+    }
+}
