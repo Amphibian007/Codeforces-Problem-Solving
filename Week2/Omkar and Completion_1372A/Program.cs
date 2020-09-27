e0fef44e01f36237d79e2828c98c1bc5a3eaecdf
Dipto Das <diptod111@gmail.com>
2020-09-27 11:29:50 +0600

    Week 2 Day 1 Commit

---
+++ Week2/Omkar and Completion_1372A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Omkar_and_Completion_1372A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            {
+                for(int i = 0; i < T; i++)
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    for (int j = 1; j <= n; j++)
+                        Console.Write(3+ " ");
+                    Console.WriteLine();
+                }
+            }
+            
+        }
+    }
+}
---
+++ Week2/Yet Another Two Integers Problem_1409A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Yet_Another_Two_Integers_Problem_1409A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                var a = input[0];
+                var b = input[1];
+                var minus = Math.Abs(a - b);
+
+                if (minus == 0)
+                    Console.WriteLine(0);
+                else if (minus <= 10)
+                    Console.WriteLine(1);
+                else
+                {
+                    if (minus % 10 == 0)
+                        Console.WriteLine(minus / 10);
+                    else
+                        Console.WriteLine((minus / 10) + 1);
+                }
+            }
+        }
+    }
+}
