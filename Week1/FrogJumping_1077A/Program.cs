f412894c3732b5b407ecb245157fc216e0df089d
Dipto Das <diptod111@gmail.com>
2020-09-27 10:13:46 +0600

    Week1 Final Commit

---
+++ Week1/FrogJumping_1077A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FrogJumping_1077A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var T = int.Parse(Console.ReadLine());
+            for (var i = 0; i < T; i++)
+            {
+                long[] input = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+                var a = input[0];
+                var b = input[1];
+                var c = input[2];
+                var step = c / 2;
+                if (c % 2 == 1)
+                    Console.WriteLine((a * (step + 1)) - b * step);
+                else
+                    Console.WriteLine(a * step - b * step);
+            }
+
+        }
+    }
+}
---
+++ Week1/Yet Another Two Integers Problem_1409A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yet_Another_Two_Integers_Problem_1409A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
