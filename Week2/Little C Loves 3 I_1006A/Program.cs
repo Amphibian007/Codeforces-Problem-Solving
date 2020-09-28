137d0317ff804bf0e5b55f0da94c10e196220e70
Dipto Das <diptod111@gmail.com>
2020-09-28 21:17:06 +0600

    Week 2 Day 2 commit

---
+++ Week2/Little C Loves 3 I_1006A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Little_C_Loves_3_I_1006A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if((n-2)%3==0)
+                Console.WriteLine($"1 2 {n-3}");
+            else
+                Console.WriteLine($"1 1 {n-2}");
+        }
+    }
+}
