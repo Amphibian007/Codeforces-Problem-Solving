10ae4ca23fafdd589ab2af552fdc7830058dba52
Dipto Das <diptod111@gmail.com>
2021-01-02 11:21:22 +0600

    Added Day 2 Code

---
+++ New Year Week1/Cards/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zero=0;
+            int one = 0;
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'z')
+                    zero++;
+                if (input[i] == 'n')
+                    one++;
+            }
+            for (int i = 0; i < one; i++)
+            {
+                Console.Write("1 ");
+            }
+            for (int i = 0; i < zero; i++)
+            {
+                Console.Write("0 ");
+            }
+           
+
+        }
+    }
+}
