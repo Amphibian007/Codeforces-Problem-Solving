4410aef5edb686121897c0c2951c433ab2a926d8
Dipto Das <diptod111@gmail.com>
2021-01-05 00:38:14 +0600

    Added day 3 and 4 codes|| 2 problems

---
+++ New Year Week1/Fafa and his Company/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fafa_and_his_Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int team = 0;
+            
+                for(int i = 2; i <= input; i++)
+                {
+                    if ((input - i) % i == 0)
+                        team++;                  
+                }
+            Console.WriteLine(team);
+
+        }
+    }
+}
---
+++ New Year Week1/VanyaAndCubes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VanyaAndCubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,sum = 0;
+            int input = int.Parse(Console.ReadLine());
+            for( n = 1; n <= 10000; n++)
+            {
+                sum += (n * (n + 1)) / 2;
+                if (sum > input)
+                    break;
+            }
+            Console.WriteLine(n-1);
+        }
+    }
+}
